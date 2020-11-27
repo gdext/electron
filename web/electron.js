d2ff98c31cc56afeeda4a6bcd6b300c3a645a8fe
GDUcrash <gducrash@gmail.com>
2020-11-27 15:41:20 +0200

    quit confirmation

--- web/electron.js
+++ web/electron.js
@@ -4,7 +4,9 @@ window.addEventListener('electronApi', e => {
     if(e.detail == 'openDevTools') ipcRenderer.invoke('openDevTools');
     else if(e.detail == 'toggleFullscreen') ipcRenderer.invoke('toggleFullscreen');
     else if(e.detail == 'reload') window.location.reload();
-    else if(e.detail == 'quit') window.close();
+    else if(e.detail == 'quit') {
+        if(confirm('Are you sure you want to quit?')) window.close();
+    }
 });
 
 ipcRenderer.on('updates', () => {
